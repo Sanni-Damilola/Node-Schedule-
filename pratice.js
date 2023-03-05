10ac0dafb5321f1f7c8ff64ef8c217786f67574a
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-05 18:29:34 +0100

    update

--- pratice.js
+++ pratice.js
@@ -2,5 +2,14 @@ const schedule = require("node-schedule");
 
 const rule = new schedule.RecurrenceRule();
 
+rule.hour = 18
+rule.minute = 20
 
 
+
+// const rule = new schedule.RecurrenceRule();
+// rule.minute = 42;
+
+// const job = schedule.scheduleJob(rule, function () {
+//   console.log("The answer to life, the universe, and everything!");
+// });
