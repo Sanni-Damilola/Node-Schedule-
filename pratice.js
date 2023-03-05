ce660b34502768327e5b05ecf1503ad2a70f2a22
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-05 17:52:02 +0100

    update

--- pratice.js
+++ pratice.js
@@ -1,9 +1,7 @@
 const file = require("node-schedule");
 
-let date = new Date("2023-03-05T16:50:00.318Z");
+let date = new Date("2023-03-05T16:52:00.318Z");
 
-const schedule =
-  (date,
-  () => {
-    console.log("done", new Date());
-  });
+const schedule = file.scheduleJob(date, () => {
+  console.log("done", new Date());
+});
