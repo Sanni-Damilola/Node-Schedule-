80dbacea5e3eafd13637c93997602b69871c4baa
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-07 21:07:16 +0100

    update

--- pratice.js
+++ pratice.js
@@ -12,9 +12,13 @@ let range = new node_schedule.Range()
 // console.log("RecurrenceRule", recurrenceRule);
 // console.log("range", range);
 
-recurrenceRule.dayOfWeek = [0, new node_schedule.Range(4, 6)]
-recurrenceRule.hour = 8
-recurrenceRule.minute = 59
+recurrenceRule.dayOfWeek = [0, new node_schedule.Range(0, 6)]
+recurrenceRule.hour = 21
+recurrenceRule.minute = 7
+
+const seeRange = node_schedule.scheduleJob( recurrenceRule,() => {
+console.log("yo");
+})
 
 
 
