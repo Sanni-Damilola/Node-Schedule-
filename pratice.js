283d40b17d4c2eb85e0183cc139c716176b99d1a
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-07 20:29:24 +0100

    update

--- pratice.js
+++ pratice.js
@@ -1,21 +1,12 @@
-const schedule = require("node-schedule");
-// console.log(schedule);
 
-// scheduleJob
-// cancelJob
-// RecurrenceRule
-// Range
-
-const date = new Date("2023-03-05T20:13:00.166Z");
-// console.log(date);
-
-const job = schedule.scheduleJob("any", "* * * * * sun", () => {
-  console.log("Hello, This is the Time", new Date());
-  schedule.cancelJob("any");
-});
+const node_schedule = require("node-schedule")
 
 
-// const 
+const check = node_schedule.scheduleJob("* * * * * *", () => {
+  console.log("Done");
+})
+
+
 
 // *    *    *    *    *    *
 // ┬    ┬    ┬    ┬    ┬    ┬
